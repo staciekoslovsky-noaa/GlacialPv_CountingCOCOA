efdcf89802a58891db19f991d87350e85b3a4da2
staciekoslovsky-noaa <stacie.koslovsky@noaa.gov>
2024-10-04 13:59:35 -0700

    Update GlacialPv_COCOA_01_PrepareData4Viame.R

--- GlacialPv_COCOA_01_PrepareData4Viame.R
+++ GlacialPv_COCOA_01_PrepareData4Viame.R
@@ -2,7 +2,7 @@
 
 # STARTING VARIABLES 
 survey_year <- 2020
-survey_id <- 'endicott_20200903_fullmosaic_1' # survey_id to be counted
+survey_id <- 'jhi_20200903_fullmosaic_1' # survey_id to be counted
 interval2keep <- 1 # keep every nth image for image review (this needs to be evaluated in QGIS before running this code)
 
 # Create functions -----------------------------------------------
@@ -36,10 +36,10 @@ setwd(wd)
 con <-   DBI::dbConnect(RPostgreSQL::PostgreSQL(),
                         dbname = Sys.getenv("pep_db"),
                         host = Sys.getenv("pep_ip"),
-                        # user = Sys.getenv("pep_admin"),
-                        # password = Sys.getenv("admin_pw"))
-                        user = Sys.getenv("pep_user"),
-                        password = Sys.getenv("user_pw"))
+                        user = Sys.getenv("pep_admin"),
+                        password = Sys.getenv("admin_pw"))
+                        # user = Sys.getenv("pep_user"),
+                        # password = Sys.getenv("user_pw"))
 
 images_all <- sf::st_read(con, query = paste0("SELECT * FROM surv_pv_gla.geo_images_footprint_network_path WHERE survey_id = \'", survey_id, "\' ORDER BY image_name ")) %>%
   mutate(dt = str_extract(image_name, "[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]_[0-9][0-9][0-9][0-9][0-9][0-9].[0-9][0-9][0-9][0-9][0-9][0-9]"))
